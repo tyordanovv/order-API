7fcab900f5e0b80744b6918ad8460628cf21299a
Tihomir Yordanov <t.yordanovv@gmail.com>
2023-03-19 23:04:33 +0200

    added basic authentication with stored localy credentials for discovery service

--- eureka-service/src/test/java/bg/tyordanovv/eureka/EurekaServerApplicationTests.java
+++ eureka-service/src/test/java/bg/tyordanovv/eureka/EurekaServerApplicationTests.java
@@ -2,6 +2,7 @@ package bg.tyordanovv.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -15,8 +16,18 @@ public class EurekaServerApplicationTests {
     @Test
     void contextLoads() {
     }
+    @Value("${app.eureka-username}")
+    private String username;
+
+    @Value("${app.eureka-password}")
+    private String password;
+
 
     @Autowired
+    void setTestRestTemplate(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate.withBasicAuth(username, password);
+    }
+
     private TestRestTemplate testRestTemplate;
 
     @Test
